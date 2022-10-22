2f782d3f946ae8b7a8aeab919092906e00eea22c
Kimbyungkwan <kqudk@naver.com>
2022-10-22 15:17:53 +0900

    feat: test

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,23 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import { useState } from 'preact/hooks';
+import preactLogo from './assets/preact.svg';
+import './app.css';
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src='/vite.svg' class='logo' alt='Vite logo' />
         </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        <a href='https://preactjs.com' target='_blank'>
+          <img src={preactLogo} class='logo preact' alt='Preact logo' />
         </a>
       </div>
       <h1>Vite + Preact</h1>
-      <div class="card">
+      <h1>변경이되나 ?</h1>
+      <div class='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -24,9 +25,9 @@ export function App() {
           Edit <code>src/app.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
+      <p class='read-the-docs'>
         Click on the Vite and Preact logos to learn more
       </p>
     </>
-  )
+  );
 }
